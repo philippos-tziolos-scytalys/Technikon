d2bed162ddbb37a37378d61b1804c731d16b6cdd
Philippos Tziolos SCYTALYS <philippos.tziolos@scytalys.com>
2023-07-12 09:42:41 +0300

    Added Lombok, jpa, web, postgres, validation dependencies

---
+++ src/main/java/com/scytalys/technikon/TechnikonApplication.java
@@ -0,0 +1,13 @@
+package com.scytalys.technikon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnikonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnikonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scytalys/technikon/TechnikonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scytalys.technikon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnikonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
