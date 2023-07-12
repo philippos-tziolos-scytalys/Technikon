428fa3a94b496f7219092f5b54b9ebc3f0534e37
kevin <kevin.rosales@scytalys.com>
2023-07-12 15:12:54 +0300

    emptied property repo it was error

--- src/main/java/com/scytalys/technikon/repository/PropertyRepository.java
+++ src/main/java/com/scytalys/technikon/repository/PropertyRepository.java
@@ -1,16 +1,8 @@
 package com.scytalys.technikon.repository;
 
 import com.scytalys.technikon.domain.Property;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PropertyRepository extends JpaRepository<Property, Long> {
-=======
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PropertyRepository extends CrudRepository<Property, Long> {
->>>>>>> origin/Pavlos
 
 }
