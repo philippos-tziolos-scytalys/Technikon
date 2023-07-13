83acd8d747cedcdcfd9ee74c5be8528ed76371f4
Philippos Tziolos SCYTALYS <philippos.tziolos@scytalys.com>
2023-07-13 13:55:18 +0300

    Added repairMapper to RepairController

--- src/main/java/com/scytalys/technikon/mapper/RepairMapper.java
+++ src/main/java/com/scytalys/technikon/mapper/RepairMapper.java
@@ -4,9 +4,15 @@ import com.scytalys.technikon.domain.Repair;
 import com.scytalys.technikon.dto.RepairDto;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface RepairMapper {
     Repair repairDtoToRepair(RepairDto repairDto);
 
     RepairDto repairToRepairDto(Repair repair);
+
+    List<Repair> repairDtoListToRepairList(List<RepairDto> repairDtoList);
+
+    List<RepairDto> repairListToRepairDtoList(List<Repair> repairList);
 }
