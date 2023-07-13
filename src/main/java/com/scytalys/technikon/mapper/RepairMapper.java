b434717a250497ad5a9590f81c07fc81dcb13026
Philippos Tziolos SCYTALYS <philippos.tziolos@scytalys.com>
2023-07-13 12:55:25 +0300

    Completed RepairMapper

--- src/main/java/com/scytalys/technikon/mapper/RepairMapper.java
+++ src/main/java/com/scytalys/technikon/mapper/RepairMapper.java
@@ -1,8 +1,12 @@
 package com.scytalys.technikon.mapper;
 
+import com.scytalys.technikon.domain.Repair;
+import com.scytalys.technikon.dto.RepairDto;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface RepairMapper {
+    Repair repairDtoToRepair(RepairDto repairDto);
 
+    RepairDto repairToRepairDto(Repair repair);
 }
