d7fd416252699ef6f5deabbedf4c538e2e589ab9
kevin <kevin.rosales@scytalys.com>
2023-07-13 10:51:32 +0300

    Implemented generics and fixed Delete user by ID

--- src/main/java/com/scytalys/technikon/controller/UserController.java
+++ src/main/java/com/scytalys/technikon/controller/UserController.java
@@ -1,9 +1,17 @@
 package com.scytalys.technikon.controller;
 
+import com.scytalys.technikon.base.BaseComponent;
+import com.scytalys.technikon.domain.BaseModel;
 import com.scytalys.technikon.domain.User;
 import com.scytalys.technikon.dto.UserDto;
+import com.scytalys.technikon.mapper.BaseMapper;
+import com.scytalys.technikon.mapper.UserMapper;
 import com.scytalys.technikon.repository.UserRepository;
+import com.scytalys.technikon.service.BaseService;
 import com.scytalys.technikon.service.UserService;
+import com.scytalys.technikon.transfer.resource.BaseResource;
+import com.scytalys.technikon.transfer.resource.UserResource;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +20,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-public class UserController {
+public class UserController extends BaseController<User, UserResource> {
 
     private final UserService userService;
-    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    protected BaseService<User, Long> getBaseService() {
+        return userService;
+    }
+
+    @Override
+    public BaseMapper<User, UserResource> getMapper() {
+        return userMapper;
+    }
 
     @PostMapping("/create")
     public ResponseEntity<UserDto> addUser(@RequestBody UserDto userDto){
@@ -27,4 +45,12 @@ public class UserController {
     public ResponseEntity<UserDto> searchUser(@PathVariable("id") Long id){
         return new ResponseEntity<>(userService.getUserById(id), HttpStatus.ACCEPTED);
     }
+
+    @DeleteMapping("delete/{id}")
+    public void delete(@PathVariable("id") final Long id) {
+        userService.deleteUser(id);
+    }
+
+
+
 }
--- src/main/java/com/scytalys/technikon/mapper/UserMapper.java
+++ src/main/java/com/scytalys/technikon/mapper/UserMapper.java
@@ -2,10 +2,11 @@ package com.scytalys.technikon.mapper;
 
 import com.scytalys.technikon.domain.User;
 import com.scytalys.technikon.dto.UserDto;
+import com.scytalys.technikon.transfer.resource.UserResource;
 import org.mapstruct.Mapper;
 
 @Mapper
-public interface UserMapper {
+public interface UserMapper extends BaseMapper<User, UserResource>{
 
     User userDtoToUser(UserDto userDto);
 
